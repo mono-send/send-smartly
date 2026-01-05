cd5f0864f62d82312a988718aeab73821084e172
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2026-01-05 22:36:50 +0200

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -61,7 +61,7 @@ const LoginPage = () => {
       }
 
       try {
-        const apiResponse = await fetch("https://internal-api.monosend.io/v1.0/auth/google", {
+        const apiResponse = await fetch("https://internal-api.monosend.io/auth/google", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
