2d2a9fae1d9754152afa2dfd8fedee7a1e2ac8c6
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2026-01-08 00:40:07 +0200

    Update AutomationsPage.tsx

--- src/pages/AutomationsPage.tsx
+++ src/pages/AutomationsPage.tsx
@@ -226,7 +226,7 @@ export default function AutomationsPage() {
       {/* Main Content */}
       <div className="flex-1 flex">
         {/* Flow Builder */}
-        <div className="flex-1 overflow-auto p-8">
+        <div className="flex-1 overflow-auto p-8 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] bg-size-[16px_16px]">
           <div className="max-w-2xl mx-auto space-y-0">
             {/* Entry Block */}
             <Card className="p-4">
