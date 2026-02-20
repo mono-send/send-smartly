f2e6b817ecc0c00a51da369b96ecca6af36847e8
Cooper <oleg.kuprovskiy@gmail.com>
2026-02-21 00:17:40 +0200

    Update ConfirmActionDialog.tsx

--- src/components/dialogs/ConfirmActionDialog.tsx
+++ src/components/dialogs/ConfirmActionDialog.tsx
@@ -41,7 +41,7 @@ export function ConfirmActionDialog({
           <AlertDialogAction
             onClick={onConfirm}
             className={cn(
-              variant === "destructive" && " h-9 lowercase bg-destructive text-destructive-foreground hover:bg-destructive/90"
+              variant === "destructive" && " h-9 bg-destructive text-destructive-foreground hover:bg-destructive/90"
             )}
           >
             {confirmLabel}
